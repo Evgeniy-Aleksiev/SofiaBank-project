2e5649cdcbbe6d04dc053478ba791321f8584823
Evgeniy <devil_98@abv.bg>
2022-04-08 18:13:48 +0300

    Fixed DB settings for deploy

--- sofia_bank/sofia_bank/settings.py
+++ sofia_bank/sofia_bank/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'sofia_bank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sofia_bank_db',
-        'USER': 'postgres',
-        'PASSWORD': '1123QwER',
-        'HOST': '127.0.0.1',
+        'NAME': 'd2v6bb39rmsrq5',
+        'USER': 'zrzsmqlfvtrqye',
+        'PASSWORD': 'fd8a3d8df3978332094951a832f1965b06d5dc97f98e51bea095ab65ba5dee00',
+        'HOST': 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
