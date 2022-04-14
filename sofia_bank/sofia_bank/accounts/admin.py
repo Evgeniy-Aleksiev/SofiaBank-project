2a8718b5e87bc71628e7b999b8ce029da4eb94be
Evgeniy <devil_98@abv.bg>
2022-04-14 16:00:00 +0300

    extends admin user

--- sofia_bank/sofia_bank/accounts/admin.py
+++ sofia_bank/sofia_bank/accounts/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from sofia_bank.accounts.models import Profile
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class SofiaBankUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    search_fields = ('username', )
+    ordering = ('username', )
+
 
 
 @admin.register(Profile)
